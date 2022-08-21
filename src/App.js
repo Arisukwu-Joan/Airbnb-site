36fe56b3ea90c40b8c54af42f52212c07158192e
arisukwu-joan <joanarisukwu@gmail.com>
2022-08-21 13:20:55 +0100

    add props initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import "./App.css";
 import Contact from "./components/Contact";
-import MainContent from "./components/MainContent";
+// import MainContent from "./components/MainContent";
 import Navbar from "./components/Navbar";
 
-function App() {
+function App(props) {
   return (
     <div className="body-container">
       <div className="container">
         <div className="App">
           <Navbar />
-          <MainContent />
-          <Contact />
+          {/* <MainContent /> */}
+          <div className="section">
+            <Contact
+              img="swim.png"
+              place="5.0 (6).USA"
+              title="life lessons with katie zaferes"
+              review="From $136 / person"
+            />
+            <Contact
+              img="wedding.png"
+              place="5.0 (30).USA"
+              title="learning wedding photograph"
+              review="From $125 / person"
+            />
+            <Contact
+              img="mountain.png"
+              place="5.0 (30).USA"
+              title="Group mountain bike"
+              review="From $50 / person"
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React from "react";
+// import swim from "../image/swim.png";
+import star from "../image/star.png";
 
-function Contact() {
+function Contact(props) {
+  console.log(props);
+  return (
+    <div className="main-img">
+      {/* <img src={`../image/${props.img}`} alt="" className="page-img" /> */}
+      <img src={"/image/swim.png"} alt="" className="page-img" />
 
-    const firstName = "joan";
-    const lastName = "bella";
-  
-    return (
-    <div>
-        <h1>Hello {firstName} {lastName}!</h1>
+      <div className="star">
+        <img src={star} alt="" className="star-img" />
+        <p className="usa">{props.place}</p>
+      </div>
+      <div className="sub-title">
+        <p className="life">{props.title}</p>
+        <p className="life-span">{props.review}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
