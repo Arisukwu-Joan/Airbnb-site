304c478245987f70914e6d6d5d07607f3a743413
arisukwu-joan <joanarisukwu@gmail.com>
2022-08-22 04:35:51 +0100

    reponsiveness initial commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,20 @@
 import React from "react";
 import star from "../images/star.png";
 
-
 function Contact(props) {
   return (
     <div className="main-img">
       {/* <img src={`../image/${props.img}`} alt="" className="page-img" /> */}
       <img src={props.img} alt="" className="page-img" />
-
-      <div className="star">
-        <img src={star} alt="" className="star-img" />
-        <p className="usa">{props.place}</p>
-      </div>
-      <div className="sub-title">
-        <p className="life">{props.title}</p>
-        <p className="life-span">{props.review}</p>
+      <div className="hero">
+        <div className="star">
+          <img src={star} alt="" className="star-img" />
+          <p className="usa">{props.place}</p>
+        </div>
+        <div className="sub-title">
+          <p className="life">{props.title}</p>
+          <p className="life-span">{props.review}</p>
+        </div>
       </div>
     </div>
   );
